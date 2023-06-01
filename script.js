e11913cc23c025d5d64cdc5719c17dd674fb4afd
Mayank Namdev <117892477+mayanknagithub@users.noreply.github.com>
2023-06-01 12:39:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,11 +6,9 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-function team(parms) {
-	let team1 = players;
+	let team = players;
+    let team1 = players;
 	let cap1 = person;
-}
-let team1 = players;
 window.players = players;
 window.person = person;
 window.team = team;
